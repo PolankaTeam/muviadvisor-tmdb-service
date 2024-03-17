e3807fc1e61606a7c7bb85a5649bd795120bdb77
Xenstris <maciek@eko-era.pl>
2024-03-17 22:35:27 +0100

    Fast API POC and TMDB integration example

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, HTTPException
 from apiCall import *
 import uvicorn
+import os
 
 app = FastAPI()
 
-
 @app.get("/queryById")
 async def root():
-    api_key = "7e59b9c230ccf64f151e491af5554fcc"
+    api_key = os.getenv('API_KEY')
     movie_id = 100 # Example movie ID: Fight Club
     movie_details = fetch_movie_details(movie_id, api_key)
     if movie_details:
@@ -15,14 +15,14 @@ async def root():
     else:
         return "Failed to fetch movie details."
 
-@app.get("/queryByMovie")
+@app.get("/freeTextSearch")
 async def query_by_movie(query: str):
-    api_key = "7e59b9c230ccf64f151e491af5554fcc"
+    api_key = os.getenv('API_KEY')
     movies = search_movie_by_keywords(api_key, query)
     if movies:
         return movies
     else:
-        return "Failed to fetch movie details."
+        return api_key
     
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
